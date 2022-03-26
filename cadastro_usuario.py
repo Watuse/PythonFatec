2b0698828f15dcf0d42b5b6d9b47e4966307fb78
Watuse <102422643+Watuse@users.noreply.github.com>
2022-03-26 16:35:57 -0300

    commit inicial
    este software tem por finalidade cadastrar usuário em uma data base

---
+++ cadastro_usuario.py
@@ -0,0 +1,9 @@
+#Desafio 1 - A - Entrada User
+#B - Comparar E-mails (Email- que você criou, com e-mail do usuário)
+
+email_que_deve_ser_adivinhado = "watuse@gmail.com"
+email = input("Digite seu e-mail: ")
+if email == email_que_deve_ser_adivinhado :
+    print ("Acesso Permitido")
+else:
+    print ("Acesso Negado")
