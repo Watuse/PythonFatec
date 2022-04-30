c4e2cf05c4f7573525367a4df71744e746785475
Aprender a configurar direito <watusemaria@gmail.com>
2022-04-30 16:14:38 -0300

    inicicommit
    Projeto de Web com Flask

---
+++ aula_5/pega-cep/entendendo_json.py
@@ -0,0 +1,23 @@
+# O que é um dicionário
+
+mario = {
+    "nome": "mario",
+    "altura": 0,
+    "poder": ["fogo", "capa"],
+    "posicao": ["abaixar", "em pe", "montado"],
+}
+
+# o que é um json
+endereco = {
+  "cep": "07793-000",
+  "logradouro": "Rua Bebedouro",
+  "complemento": "(Prq Jaraguá)",
+  "bairro": "Panorama (Polvilho)",
+  "localidade": "Cajamar",
+  "uf": "SP",
+  "ibge": "3509205",
+  "gia": "2410",
+  "ddd": "11",
+  "siafi": "6285"
+}
+
